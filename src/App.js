1e9f7001e1b8d21aa16676f441be4a8efb195d4e
Turkay Tunc <turkaytunc00@gmail.com>
2020-01-28 21:00:18 +0300

    update app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">heyo</div>;
+  return <div className="App">Sometimes it becomes harder!!</div>;
 }
 
 export default App;
