df18d20e55d3074d0c0c4f6e06574429b39b95a8
Turkay Tunc <turkaytunc00@gmail.com>
2020-01-28 20:52:40 +0300

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">hey</div>;
+  return <div className="App">Something</div>;
 }
 
 export default App;
