632355e4d17a1a71cbb92dbdfa2aa8588aa3fb3f
Turkay Tunc <turkaytunc00@gmail.com>
2020-01-28 20:44:52 +0300

    Updates

---
+++ precache-manifest.67d48e5bc7ae22b66ba013e99f58c48b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c0911ecf1b63d06f313b544428ef2b6",
+    "url": "/react-todos/index.html"
+  },
+  {
+    "revision": "89f8d524b879caaeb48c",
+    "url": "/react-todos/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "35b3b1c91d71b182a0b3",
+    "url": "/react-todos/static/js/2.2fe87da1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todos/static/js/2.2fe87da1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "89f8d524b879caaeb48c",
+    "url": "/react-todos/static/js/main.473d55db.chunk.js"
+  },
+  {
+    "revision": "70858a7b2d7d6f03f544",
+    "url": "/react-todos/static/js/runtime-main.21591a46.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todos/static/media/logo.5d5d9eef.svg"
+  }
+]);
