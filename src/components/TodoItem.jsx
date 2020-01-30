f463990e17c04ce66ea567e5b6127c55b517236a
Turkay Tunc <turkaytunc00@gmail.com>
2020-01-30 14:52:04 +0300

    Added onchange

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -5,7 +5,11 @@ class TodoItem extends Component {
   render() {
     return (
       <div className="todo-item">
-        <input type="checkbox" checked={this.props.item.completed} />
+        <input
+          type="checkbox"
+          checked={this.props.item.completed}
+          onChange={() => console.log("changed")}
+        />
         <p className="todo-item-paragraph">{this.props.item.title}</p>
       </div>
     );
