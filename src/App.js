53ea4c133d27ec8527e42c7ce62f809cc4906743
Turkay Tunc <turkaytunc00@gmail.com>
2020-01-30 11:25:53 +0300

    Added data file and styles files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
+import { TodoItem } from "./components/TodoItem";
+import { TodoData } from "./TodoData";
 
-function App() {
-  return <div className="App">Sometimes it becomes harder!!</div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: TodoData
+    };
+  }
+
+  render() {
+    const todoItems = this.state.todos.map(item => (
+      <TodoItem key={item.id} item={item}></TodoItem>
+    ));
+    return (
+      <div className="app">
+        <div className="todo-list">{todoItems}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "../css/TodoItem.css";
+
+class TodoItem extends Component {
+  render() {
+    return (
+      <div className="todo-item">
+        <input type="checkbox" checked={this.props.item.completed} />
+        <p className="todo-item-paragraph">{this.props.item.title}</p>
+      </div>
+    );
+  }
+}
+
+export { TodoItem };
